d23b8347e933658f3f7411b115aaf738136d771f
joonamkinen <joona.kristian@gmail.com>
2023-10-10 00:24:16 +0300

    Assingment 3 valmis

--- Assignment_3/app/src/main/java/com/example/assignment_3/MainActivity.java
+++ Assignment_3/app/src/main/java/com/example/assignment_3/MainActivity.java
@@ -1,13 +1,63 @@
 package com.example.assignment_3;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    private int randomNumber;
+    private String date;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState != null) {
+            date = savedInstanceState.getString("Date");
+            Toast.makeText(this, date, Toast.LENGTH_SHORT).show();
+        }
+
+        TextView randomNumberTextView = findViewById(R.id.randomNumberTextView);
+
+        Random random = new Random();
+        Handler handler = new Handler();
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                randomNumber = random.nextInt(100);
+                randomNumberTextView.setText("Random Number: " + randomNumber);
+                handler.postDelayed(this, 1000); // 1000 milliseconds = 1 second
+            }
+        };
+        handler.post(runnable);
+    }
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        outState.putInt("Random", randomNumber);
+        date = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault()).format(new Date());
+        outState.putString("Date", date);
+        super.onSaveInstanceState(outState);
+    }
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        int latestRandomNumber = savedInstanceState.getInt("Random");
+        TextView latestRandomNumberView = findViewById(R.id.latestRandomNumberTextView);
+        latestRandomNumberView.setText("Latest Random Number: " + latestRandomNumber);
     }
 }
